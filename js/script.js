2cc4c7e83403bd61d34d7fb49c0e4ca21de97603
lwinmoeaung-springarts <blackplayerbk@gmail.com>
2022-08-30 16:04:41 +0630

    fix data & default color

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 /* ============================== typing animation ============================ */
 var typed = new Typed(".typing",{
-    strings:["","Web Designer","Web Developer","Graphic Designer","YouTuber"],
+    strings:["","Web Developer","Challenger","Creater" ," Limited Edition"],
     typeSpeed:100,
     BackSpeed:60,
     loop:true
